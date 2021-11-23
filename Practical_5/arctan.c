cbd1e224d052d40611a07f10e677ca34fede4813
CNS1mple <cherishlsy.me@gmail.com>
2021-11-23 17:25:16 +0000

    Practical 5 : more comments

--- Practical_5/arctan.c
+++ Practical_5/arctan.c
@@ -5,6 +5,7 @@ double delta;
 double array1[100005];
 double array2[100005];
 
+// Maclaurin series
 double artanh1(double x) {
 
     double sum = 0.0;
@@ -12,12 +13,13 @@ double artanh1(double x) {
     for(int i = 0;; i++) {
         double ele = pow(x, 1+2*i) / (2*i + 1);
         sum += ele;
-        if(fabs(ele) <  delta) break;  
+        if(fabs(ele) <  delta) break;  // loop stops when elements is smaller than the precision.
     }
     return sum;
 
 }
 
+// natural logarithms
 double artanh2(double x) {
    return 0.5*(log(1+x) - log(1-x));
 }
@@ -29,7 +31,7 @@ int main(void) {
 
     int len = 0;
 
-    for(double i = -0.9; i <= 0.9; i+= 0.01) {
+    for(double i = -0.9; i <= 0.9; i+= 0.01) { // x ∈ [−0.9,0.9] and sampled every 0.01
         array1[len] = artanh1(i);
         array2[len] = artanh2(i);
         printf("The difference between tan[%.3lf] and tan[%.3lf] is %.10lf", i, i, fabs(array1[len] - array2[len]));
