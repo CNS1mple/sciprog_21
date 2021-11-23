b4a1d4127346cac2015d0ca2dad641fa8c1879dc
CNS1mple <cherishlsy.me@gmail.com>
2021-11-23 17:20:34 +0000

    Practical 5: add comments

--- Practical_5/Fibonacci.c
+++ Practical_5/Fibonacci.c
@@ -3,13 +3,14 @@
 
 
 void Fibonacci(int n) {
-    int f[100005];
+    int f[100005]; // just in case we don't meet "array out of bounds"
     f[0] = 0;
     f[1] = 1;
+    //iteratively calculate the value
     for(int i = 2; i < n; i++) {
         f[i] = f[i - 1] + f[i - 2];
     }
-
+    // print the series 
     for(int i = 0; i < n; i++) {
         printf("%d \n", f[i]);
     }
@@ -17,7 +18,7 @@ void Fibonacci(int n) {
 
 int main(void) {
     int n;
-    printf("Please input a positive number:");
+    printf("Please input a positive number:\n");
     scanf("%d", &n);
     Fibonacci(n);
 }
