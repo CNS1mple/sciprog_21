9f75066a0646eeafd282856cd27c69bdc42e1a6c
CNS1mple <cherishlsy.me@gmail.com>
2021-11-23 17:32:09 +0000

    Practical 7: add comments

--- Practical_7/main.c
+++ Practical_7/main.c
@@ -4,22 +4,26 @@
 
 int *allocatearray(int N) {
     int *p;
+    // allocating space to arrays
     p = (int *)malloc(N * sizeof(int));
     return p;
 }
 
+// fill an array with 1s
 void fillwithones(int *p, int n) {
     for(int i = 0; i < n; i++) {
         p[i] = 1;
     }
 }
 
+// print the array
 void printarray(int *p, int n) {
     for(int i = 0; i < n; i++) {
         printf("%d\n", p[i]);
     }
 }
 
+// free space
 void free_m(int *p) {
     free(p);
 }
